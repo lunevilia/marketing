e311f09bf0d9a14caae287a43edc6562e5f2a260
이창우 <cwadven@naver.com>
2020-06-05 11:33:20 +0900

    회원 아닌 사람은 좋아요 누르는것 안보이게git add .

--- usingform/templatetags/post_extras.py
+++ usingform/templatetags/post_extras.py
@@ -5,8 +5,8 @@ register = template.Library()
 
 @register.filter
 def _checking(obj, user):
-    getProfile = Profile.objects.get(user__username=user) #안됬는데 보니깐 슈퍼유저는 Profile이 안만들어져서 찾지를 못하는 것이였음
     try:
+        getProfile = Profile.objects.get(user__username=user) #안됬는데 보니깐 슈퍼유저는 Profile이 안만들어져서 찾지를 못하는 것이였음
         CommentLike.objects.get(post=obj, author=getProfile)
         return "좋아요 취소"
     except:
