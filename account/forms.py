cbb698e8acb65b3704c0c0b418253d42f1d57ce5
이창우 <cwadven@naver.com>
2020-06-09 11:08:10 +0900

    개인정보 수정에 있는 이미지에 나타나는 글자 한글로 바꾸기

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import *
+from django.forms.widgets import ClearableFileInput
+
+class MyClearableFileInput(ClearableFileInput):
+    initial_text = '현재 사진'
+    input_text = '사진 바꾸기'
+    clear_checkbox_label = '지우기'
 
 class SignupForm(forms.ModelForm):
+    
     class Meta:
         model = Profile
         fields = ('Name', 'Image', 'Email',)
@@ -27,6 +34,7 @@ class ModifyForm(forms.ModelForm):
         }
         widgets = {
             'Name': forms.TextInput(attrs={'id': 'nickname_id', 'onchange':"rematch(this.id)", }),
+            'Image': MyClearableFileInput,
         }
         error_messages = {
             'Name': {
