4263fb329d50650af3c6ab6090aa6e0146efd33d
이창우 <cwadven@naver.com>
2020-06-15 00:49:31 +0900

    nav바 디자인 추가 및 커뮤니티 드롭다운으로 들어올 수 있도록 설정

--- account/templatetags/information_extras.py
+++ account/templatetags/information_extras.py
@@ -2,6 +2,7 @@
 from django import template
 from usingform.models import CommentLike, Like, Favorite
 from account.models import Commentalert, Profile
+from category.models import Category
 register = template.Library()
 
 # Board 좋아요 용 확인
@@ -18,3 +19,8 @@ def alert_checking(obj , user):
     except:
         Commentalert.objects.create(profile=getProfile, recent=0)
         return '<svg class="svg-icon" enable-background="new 0 0 512 512" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m460.357 344.286h-15c-29.697 0-53.857-24.16-53.857-53.857v-120.542c0-50.626-37.431-92.67-86.071-99.92v-20.538c-.001-27.255-22.174-49.429-49.429-49.429s-49.429 22.174-49.429 49.429v20.538c-48.64 7.25-86.071 49.294-86.071 99.92v120.542c0 29.697-24.16 53.857-53.857 53.857h-15v98.856h154.928v19.429c0 27.255 22.174 49.429 49.429 49.429s49.429-22.174 49.429-49.429v-19.429h154.929v-98.856zm-204.357-314.286c10.713 0 19.429 8.716 19.429 19.429v19.429h-38.857v-19.429c-.001-10.713 8.715-19.429 19.428-19.429zm-105.5 260.429v-120.542c0-39.166 31.864-71.029 71.03-71.029h68.941c39.166 0 71.029 31.863 71.029 71.029v120.542c0 20.489 7.397 39.277 19.647 53.857h-250.294c12.25-14.581 19.647-33.368 19.647-53.857zm124.928 172.142c0 10.713-8.716 19.429-19.429 19.429s-19.429-8.716-19.429-19.429v-19.429h38.857v19.429zm154.929-49.428h-348.714v-38.856h348.714z"/></svg>'
+
+@register.filter
+def category_get(obj):
+    all_category = Category.objects.all()
+    return all_category
