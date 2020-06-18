7f9069db4a5ce8c09a0beb7d3ba0edc474c92134
이창우 <cwadven@naver.com>
2020-06-18 11:02:18 +0900

    게시판 순서 옮기기 만듦

--- category/forms.py
+++ category/forms.py
@@ -10,7 +10,10 @@ class CategoryForm(forms.ModelForm):
                 'unique': "동일한 게시판이 존재 합니다!! 다른 게시판을 입력해주세요!",
             },
         }
-        labels = { 'board_name': '게시판 추가'}
+        labels = { 
+            'board_name': '게시판 추가',
+            'important' : '게시판 순서'
+            }
         #help_texts = { 'title': '필수 사항 입니다!', 'body':'내용을 입력해주세요!'}
         # widgets = {
         #     'title': forms.TextInput(attrs={'class': 'title'}),
--- category/models.py
+++ category/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     board_name = models.CharField(max_length=50, unique=True)
+    important = models.IntegerField(default=1)
+
+    class Meta:
+        ordering = ['important', ]
 
     def __str__(self):
         return self.board_name
